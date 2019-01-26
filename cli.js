b4f7699b307053468ad6adae69d16076da9f5161
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2019-01-26 18:59:17 +0100

    v10: exit from once mode

--- cli.js
+++ cli.js
@@ -2,7 +2,9 @@
 var argv = require("minimist")(process.argv.slice(2));
 const { once, continuous } = require("./index");
 
-(argv.continous ? continuous : once)(argv).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+(argv.continuous ? continuous : once)(argv)
+  .then(() => process.exit(0))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
